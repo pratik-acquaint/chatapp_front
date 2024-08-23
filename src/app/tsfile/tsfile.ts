865670cadc2d87d731128a8dfd1e069800453404
mobidev86 <mobidev86@gmail.com>
2024-08-23 11:51:31 +0530

    add ts file

---
+++ src/app/tsfile/tsfile.ts
@@ -0,0 +1,26 @@
+
+interface User {
+    id: Number,
+    name: String,
+    gender: boolean,
+    birthdate: String,
+    age: any
+}
+
+let apiResponse: User = {
+    id: 0,
+    name: "",
+    gender: true,
+    birthdate: "",
+    age: null,
+}
+
+console.log(apiResponse);
+
+
+
+
+
+
+
+
